e110facf3fdf678b7cdde897e7c4d74b77ebe5a6
Himawan Ihza P <80901861+ihzapradana@users.noreply.github.com>
2022-05-14 17:11:26 +0700

    update keranjang dikit

--- lib/Keranjang_page.dart
+++ lib/Keranjang_page.dart
@@ -10,21 +10,28 @@ class keranjang extends StatefulWidget {
 class _keranjangState extends State<keranjang> {
   @override
   Widget build(BuildContext context) {
+    final tinggi = MediaQuery.of(context).size.height;
+    final lebar = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Color(0xFFECC80A),
         centerTitle: true,
         title: Text("Keranjang", style: TextStyle(color: Colors.black)),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          )
-        ],
       ),
-      body: Column(
-        children: [],
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(15, 30, 15, 30),
+              height: tinggi * 0.55,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 197, 197, 197),
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+            )
+          ],
+        ),
       ),
     );
   }
